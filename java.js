d9854aa2acb25bb286067ca53ba37e6fe241c8ad
Riveratours <enmanuelrodri02@gmail.com>
2025-02-27 21:25:38 -0400

    Delete java.js

--- java.js
+++
@@ -1,7 +0,0 @@
-// Toggle hamburger menu for mobile
-const hamburger = document.getElementById('hamburger');
-const menu = document.querySelector('nav .menu');
-
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
